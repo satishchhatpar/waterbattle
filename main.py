726fa8db3fa73882e5726fd11d5c5ec8731a990c
satishchhatpar <satish.chhatpar@gmail.com>
2021-10-02 07:58:11 +0000

    always throw commit

--- main.py
+++ main.py
@@ -30,7 +30,7 @@ moves = ['F', 'T', 'L', 'R']
 def move():
     request.get_data()
     logger.info(request.json)
-    return moves[random.randrange(len(moves))]
+    return moves[1]
 
 if __name__ == "__main__":
   app.run(debug=False,host='0.0.0.0',port=int(os.environ.get('PORT', 8080)))
