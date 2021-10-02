f287fbbde98541a1b35a3db35235b1c700bd0a32
satishchhatpar <satish.chhatpar@gmail.com>
2021-10-02 10:12:11 +0000

    show DIMS

--- main.py
+++ main.py
@@ -29,7 +29,7 @@ moves = ['F', 'T', 'L', 'R']
 @app.route("/", methods=['POST'])
 def move():
     jsondata = request.get_json(force=True)
-    str1 = ''.join(jsondata['arena']['dims'])
+    str1 = ''.join(jsondata['arena'][0])
     logger.info("DIMS is = " + str1)
     #logger.info(request.json)
     return moves[1]
