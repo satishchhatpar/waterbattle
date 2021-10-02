fa53be3cddeb507eb00871f60bd4f20b69f709a6
satishchhatpar <satish.chhatpar@gmail.com>
2021-10-02 19:38:53 +0530

    adding pyjq

--- main.py
+++ main.py
@@ -13,7 +13,7 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 # Water Battle , GCP Serverless Hackathon
-
+#adding pyjy
 
 import os
 import logging
