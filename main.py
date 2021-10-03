6024c738b87c5cd4c885ac498165390bcd811e4f
satishchhatpar <satish.chhatpar@gmail.com>
2021-10-03 11:27:25 +0530

    searching using jmespath

--- main.py
+++ main.py
@@ -19,21 +19,29 @@ def move():
     x = jmespath.search('arena.state."https://python-bot-bl3rxjcqjq-as.a.run.app".x', jsondata)
     y = jmespath.search('arena.state."https://python-bot-bl3rxjcqjq-as.a.run.app".y', jsondata)
     direction = jmespath.search('arena.state."https://python-bot-bl3rxjcqjq-as.a.run.app".direction', jsondata)
-    print(x)
-    print(y)
-    print(direction)
+    #print(x)
+    #print(y)
+    #print(direction)
     x1 = x - 1
     x2 = x + 1
     y1 = y - 1
     y2 = y + 1
-    print(x1)
-    print(x2)
-    print(y1)
-    print(y2)
+    #print(x1)
+    #print(x2)
+    #print(y1)
+    #print(y2)
 
-    print(type(jmespath.search('arena.state.*.x', jsondata)))
-    print(bool1)
-    return moves[random.randrange(len(moves))]
+    #print(type(jmespath.search('arena.state.*.x', jsondata)))
+    listx = jmespath.search('arena.state.*.x', jsondata)
+    listy = jmespath.search('arena.state.*.y', jsondata)
+    if (x1 in listx):
+        return 'T'
+    if (x2 in lixtx):
+        return 'T'
+    if (y1 in listy):
+        return 'T'
+    if (y2 in listy):
+        return 'T'
      
 if __name__ == "__main__":
   app.run(debug=False,host='0.0.0.0',port=int(os.environ.get('PORT', 8080)))
