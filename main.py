1da44313555b189246b2ce2281ce3b2b3a905db4
satishchhatpar <satish.chhatpar@gmail.com>
2021-10-03 10:30:27 +0530

    searching using jmespath

--- main.py
+++ main.py
@@ -26,7 +26,7 @@ from flask import Flask, request
 
 app = Flask(__name__)
 moves = ['F', 'T', 'L', 'R']
-me = "https://python-bot-fwutkkw3ka-as.a.run.app"
+me = https://python-bot-bl3rxjcqjq-as.a.run.app 
 
 @app.route("/", methods=['POST'])
 def move():
@@ -34,8 +34,12 @@ def move():
     #print(jmespath.search('arena.dims[0]', jsondata))
     #print(jmespath.search('arena.dims.state[*]."https://python-bot-fwutkkw3ka-as.a.run.app"[0]', jsondata))
     
-    print(jmespath.search('arena.dims.state[?"https://python-bot-fwutkkw3ka-as.a.run.app"].x', jsondata))
-    
+    x = jmespath.search('arena.state."https://python-bot-bl3rxjcqjq-as.a.run.app".x', jsondata)
+    y = jmespath.search('arena.state."https://python-bot-bl3rxjcqjq-as.a.run.app".y', jsondata)
+    direction = jmespath.search('arena.state."https://python-bot-bl3rxjcqjq-as.a.run.app".direction', jsondata)
+    print(x)
+    print(y)
+    print(direction)
     
     return moves[random.randrange(len(moves))]
 
